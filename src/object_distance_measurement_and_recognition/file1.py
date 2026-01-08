e5bef58f96d4773977c744659dec92dedb7cb49b
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2026-01-08 20:09:44 +0530

    fix imports

--- src/object_distance_measurement_and_recognition/file1.py
+++ src/object_distance_measurement_and_recognition/file1.py
@@ -1,27 +1,36 @@
 import cv2
-from realsense_camera import *
-from mask_rcnn import *
+from .realsense_camera import RealsenseCamera
+from .mask_rcnn import MaskRCNN
 
-# Load Realsense camera and Mask R-CNN
-rs = RealsenseCamera()
-mrcnn = MaskRCNN()
+if __name__ == "__main__":
+    # Load Realsense camera and Mask R-CNN
+    rs = RealsenseCamera()
+    mrcnn = MaskRCNN()
 
-while True:
-    # Get frame in real time from Realsense camera
-    ret, bgr_frame, depth_frame = rs.get_frame_stream()
+    while True:
+        # Get frame in real time from Realsense camera
+        ret, bgr_frame, depth_frame = rs.get_frame_stream()
 
-    point_x, point_y = 250, 100
-    distance_mm = depth_frame[point_y, point_x]
+        point_x, point_y = 250, 100
+        distance_mm = depth_frame[point_y, point_x]
 
-    cv2.circle(bgr_frame, (point_x, point_y), 8, (0, 0, 255), -1)
-    cv2.putText(bgr_frame, "{} mm".format(distance_mm), (point_x, point_y - 10), 0, 1, (0, 0, 255), 2)
+        cv2.circle(bgr_frame, (point_x, point_y), 8, (0, 0, 255), -1)
+        cv2.putText(
+            bgr_frame,
+            "{} mm".format(distance_mm),
+            (point_x, point_y - 10),
+            0,
+            1,
+            (0, 0, 255),
+            2,
+        )
 
-    cv2.imshow("Depth Frame", depth_frame)
-    cv2.imshow("Bgr frame", bgr_frame)
+        cv2.imshow("Depth Frame", depth_frame)
+        cv2.imshow("Bgr frame", bgr_frame)
 
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
 
 # point_x, point_y = 250, 100
 # distance_mm = depth_frame[point_x, point_y]
--- src/object_distance_measurement_and_recognition/measure_object_distance.py
+++ src/object_distance_measurement_and_recognition/measure_object_distance.py
@@ -1,32 +1,31 @@
-
 import cv2
-from realsense_camera import *
-from mask_rcnn import *
+from .realsense_camera import RealsenseCamera
+from .mask_rcnn import MaskRCNN
 
-# Load Realsense camera
-rs = RealsenseCamera()
-mrcnn = MaskRCNN()
+if __name__ == "__main__":
+    # Load Realsense camera
+    rs = RealsenseCamera()
+    mrcnn = MaskRCNN()
 
-while True:
-	# Get frame in real time from Realsense camera
-	ret, bgr_frame, depth_frame = rs.get_frame_stream()
+    while True:
+        # Get frame in real time from Realsense camera
+        ret, bgr_frame, depth_frame = rs.get_frame_stream()
 
-	# Get object mask
-	boxes, classes, contours, centers = mrcnn.detect_objects_mask(bgr_frame)
+        # Get object mask
+        boxes, classes, contours, centers = mrcnn.detect_objects_mask(bgr_frame)
 
-	# Draw object mask
-	bgr_frame = mrcnn.draw_object_mask(bgr_frame)
+        # Draw object mask
+        bgr_frame = mrcnn.draw_object_mask(bgr_frame)
 
-	# Show depth info of the objects
-	mrcnn.draw_object_info(bgr_frame, depth_frame)
+        # Show depth info of the objects
+        mrcnn.draw_object_info(bgr_frame, depth_frame)
 
+        cv2.imshow("depth frame", depth_frame)
+        cv2.imshow("Bgr frame", bgr_frame)
 
-	cv2.imshow("depth frame", depth_frame)
-	cv2.imshow("Bgr frame", bgr_frame)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
 
-	key = cv2.waitKey(1)
-	if key == 27:
-		break
-
-rs.release()
-cv2.destroyAllWindows()
+    rs.release()
+    cv2.destroyAllWindows()
